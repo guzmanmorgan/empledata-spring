b23f192ab34ede599e47489478f29a796b4e875c
Manuel <manuelguzmanvilches@gmail.com>
2021-09-10 17:53:37 -0300

    Commit inicial

---
+++ empledata/src/main/java/com/mguzman/empledata/EmpledataApplication.java
@@ -0,0 +1,13 @@
+package com.mguzman.empledata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpledataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpledataApplication.class, args);
+	}
+
+}
---
+++ empledata/src/test/java/com/mguzman/empledata/EmpledataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mguzman.empledata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpledataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
