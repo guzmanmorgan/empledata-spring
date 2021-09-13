358011b66448a66834466058d42ff1155f84d4c8
Manuel <manuelguzmanvilches@gmail.com>
2021-09-13 19:44:47 -0300

    [ADD] agregando modelos Usuario, Rol y ResetToken

---
+++ empledata/src/main/java/com/mguzman/empledata/model/Usuario.java
@@ -0,0 +1,80 @@
+package com.mguzman.empledata.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+	
+	@Id
+    private Integer idUsuario;
+
+    @Column(name = "nombre")
+    private String userName;
+
+    @Column(name = "clave")
+    private String password;
+
+    @Column(name = "estado")
+    private boolean estado;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "usuario_rol",
+            joinColumns = @JoinColumn(name = "id_usuario", referencedColumnName = "idUsuario"),
+            inverseJoinColumns = @JoinColumn(name = "id_rol", referencedColumnName = "idRol"))
+    private List<Rol> rols;
+
+	public Integer getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Integer idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
+	}
+
+	public List<Rol> getRols() {
+		return rols;
+	}
+
+	public void setRols(List<Rol> rols) {
+		this.rols = rols;
+	}
+    
+    
+    
+
+}
